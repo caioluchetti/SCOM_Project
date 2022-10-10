b225a68110e733db0dabc38e154a3d0fac71ca81
tigao123 <tcolombari123@gmail.com>
2022-10-10 14:48:05 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to my ass.
+          Edit <code>src/App.js</code> and save to my dick.
         </p>
         <a
           className="App-link"
