9fb66bb85d3ec05d3ca2eb1da0be2aa5f7dae9bb
Cortada <caio.cortada@unesp.br>
2022-10-10 14:23:34 -0300

    Feat: Setup Inicial

---
+++ src/pages/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+function App() {
+
+  
+  return (
+   
+
+  );
+}
+
+export default App;
