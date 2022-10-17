607ab6f9f83cee0a5e3df5f2700a82b5b8eafe9c
Santiago <s.teixeira@dinamicaengjr.com>
2022-10-17 14:22:42 -0300

    background img

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Rotas from './Routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode style={{ margin: 0, padding: 0 }}>
     <Rotas />
   </React.StrictMode>
 );
