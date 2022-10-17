d16efe3a52d784f44325ae19d17d6af32645c8f7
Cortada-PC <caio.cortada@unesp.br>
2022-10-17 02:14:05 -0300

    feat: mapa

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 
 import Main from './pages/App'
+import Windhelm from './pages/cidades/Windhelm'
 
 function Rotas(){
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Main/>} />
+                <Route path="/Windhelm" element={<Windhelm/>}/>
                
             </Routes>
 
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,16 +1,30 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './styles.css';
+import map from '../images/map.jpg'
 
 
 function App() {
-
+    const navigate = useNavigate();
+    const Windhelmnav = () => {
+        navigate('/Windhelm');
+      };
 
     return (
         <>
-            <div className='container'>
-                Oi!
+            <div className='mapa'>
+            <div className='Windhelm'>
+                <div className='Windhelm-content' onClick={Windhelmnav}>
+                    Clique para acessar a Lore dessa Cidade
+                </div>
             </div>
+
+            <img  alt='a' width={'100%'} height={'100%'} src={map}/>
+
+
+
+            </div>
+           
         </>
 
 
