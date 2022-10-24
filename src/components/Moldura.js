1c339f30dfad6e53b1ed96397117b161d2084b35
tigao123 <tcolombari123@gmail.com>
2022-10-24 14:29:31 -0300

    Moldura

--- src/components/Moldura.js
+++ src/components/Moldura.js
@@ -1,2 +1,9 @@
 import React from "react";
 
+export default function Moldura(props){
+    return(
+        <div>
+            <img src="props"/>
+        </div>
+    )
+}
--- src/pages/Bestiario.js
+++ src/pages/Bestiario.js
@@ -2,6 +2,7 @@ import React from "react";
 import './styles.css'
 import json from '../databaseBestiario.json'
 import Botao from "../components/Botao";
+import Moldura from "../components/Moldura";
 
 export default function Bestiario() {
 
@@ -9,7 +10,7 @@ console.log('json :>> ', json.titulo);
 
     return (
         <div className="body">
-            <img src={json.foto}/>
+            <Moldura imagem={json.foto} />
         </div>
     )
 }
