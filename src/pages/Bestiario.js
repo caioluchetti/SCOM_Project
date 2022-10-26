64637d1f296fd9e5997588b0d249082ce4648324
Sants <santiago.metadroid@gmail.com>
2022-10-26 03:25:14 -0300

    Consertando cagada

--- src/pages/Bestiario.js
+++ src/pages/Bestiario.js
@@ -8,25 +8,35 @@ import Moldura from "../components/Moldura";
 
 export default function Bestiario() {
 
-    console.log('json :>> ', json.titulo);
-
-    const imagens = [
-            {
-                img: "imagem 1"
-            },
-            {
-                img: "imagem 2"
-            },
-            {
-                img: "imagem 3"
-            }
-        ]
-
+console.log('json :>> ', json.titulo);
 
     return (
 
         <div className="body">
-            <img src={json.foto}/>
+            {/* foto da criatura */}
+            <div className="ImgOnPage">
+            <Moldura imagem={json.foto}/>
+            </div>
+
+            {/* nome da criatura */}
+            <div className="nome">
+                <p>
+                {json.nome}
+                </p>
+            </div>
+
+            {/* descrição da criatura */}
+            <div className="descricao">
+                <p>
+                {json.descricao}
+                </p>
+            </div>
+
+            <div className="combate">
+                <p>
+                {json.combate}
+                </p>
+            </div>
         </div>
     )
 }
