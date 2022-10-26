df2bffa5c61a10421e72578a80a47a0180d467cb
Sants <santiago.metadroid@gmail.com>
2022-10-26 03:41:11 -0300

    Prorótipo input

--- src/components/BarraPesquisa.js
+++ src/components/BarraPesquisa.js
@@ -9,20 +9,20 @@ export default function BarraPesquisa(props) {
         <>
             <div>
                 <div className="containerInput">
+                    <div className="trianguloE">
+                    </div>
+                    <div className="bordaTrianguloE">
+                    </div>
                     <div className="containerTrianguloE">
-                        <div className="bordaTrianguloE">
-                            <div className="trianguloE">
-                            </div>
-                        </div>
                     </div>
                     <input className="inputBusca" />
-                    <div className="trianguloD"></div>
-                    <div className="containerTrianguloD">
-                        <div className="bordaTrianguloD">
-                            <div className="trianguloD">
-                            </div>
-                        </div>
+                    <div className="trianguloD">
                     </div>
+                    <div className="bordaTrianguloD">
+                    </div>
+                    <div className="containerTrianguloD">
+                    </div>
+                    
                 </div>
             </div>
         </>
--- src/pages/Bestiario.js
+++ src/pages/Bestiario.js
@@ -3,6 +3,7 @@ import './styles.css'
 import json from '../databaseBestiario.json'
 import Botao from "../components/Botao";
 import Moldura from "../components/Moldura";
+import BarraPesquisa from "../components/BarraPesquisa";
 // import MusicaCombate from "../components/MusicaCombate";
 // import combate from '../audio/soundtrack.mp3';
 
@@ -13,6 +14,8 @@ console.log('json :>> ', json.titulo);
     return (
 
         <div className="body">
+            <BarraPesquisa/>
+            
             {/* foto da criatura */}
             <div className="ImgOnPage">
             <Moldura imagem={json.foto}/>
